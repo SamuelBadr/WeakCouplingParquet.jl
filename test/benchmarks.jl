547f3cc4c344b7ba8f0c1f6259d31d7095d2f709
Samuel Badr <samuel.badr@gmail.com>
2025-05-27 13:27:37 +0200

    fix normalization?

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -2,8 +2,8 @@
     abstol = 1e-3
     reltol = 1e-3
 
-    @time WeakCouplingParquet.phi2_d(u, mu, beta, v, vp, w, k, kp, q; abstol, reltol)
-    @time WeakCouplingParquet.phi2_m(u, mu, beta, v, vp, w, k, kp, q; abstol, reltol)
-    @time WeakCouplingParquet.phi2_s(u, mu, beta, v, vp, w, k, kp, q; abstol, reltol)
-    @time WeakCouplingParquet.phi2_t(u, mu, beta, v, vp, w, k, kp, q; abstol, reltol)
+    @time WeakCouplingParquet.phi2_d(u, mu, beta, v, vp, w, k, kp, q)
+    @time WeakCouplingParquet.phi2_m(u, mu, beta, v, vp, w, k, kp, q)
+    @time WeakCouplingParquet.phi2_s(u, mu, beta, v, vp, w, k, kp, q)
+    @time WeakCouplingParquet.phi2_t(u, mu, beta, v, vp, w, k, kp, q)
 end
